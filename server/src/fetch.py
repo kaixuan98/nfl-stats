cc722d7d1fb74194cfa24c798db7e27c8b67f896
averychin7 <avery.chin@tbcollege.com>
2025-01-12 15:51:13 -0500

    split up function to scrap data and table data

--- server/src/fetch.py
+++ server/src/fetch.py
@@ -1,10 +1,20 @@
 import requests 
 from bs4 import BeautifulSoup
 
-# link to raw data
-def get_single_url_table(link):
+# {fetch date , found tables}
+
+def get_data_soup(link): 
     data = requests.get(link).text
     soup = BeautifulSoup(data, 'html.parser')
+    return soup
+
+def get_date(soup): 
+   found_date = soup.find('input', class_='custom-date')
+   return found_date
+
+# link to raw data
+# TODO get the date and season 
+def get_table(soup):
     found_table = soup.find('table', class_='tr-table datatable scrollable')
     return found_table
 
